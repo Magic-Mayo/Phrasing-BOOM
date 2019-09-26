dcfb87478f4ed49128700dbb9c6bb1208060d7f2
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-26 01:52:59 -0700

    Updates

---
+++ precache-manifest.2299f8ed1bc236d7d0abe6370fd4cec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74b7a4886f85d076145bc791fb24b2d",
+    "url": "/Phrasing-BOOM/index.html"
+  },
+  {
+    "revision": "3a466a6e8fa0f0488ffb",
+    "url": "/Phrasing-BOOM/static/css/main.10bbb2b4.chunk.css"
+  },
+  {
+    "revision": "efea382185214e5229b4",
+    "url": "/Phrasing-BOOM/static/js/2.0e6a9ffe.chunk.js"
+  },
+  {
+    "revision": "3a466a6e8fa0f0488ffb",
+    "url": "/Phrasing-BOOM/static/js/main.79031404.chunk.js"
+  },
+  {
+    "revision": "4c1601915509a29c55df",
+    "url": "/Phrasing-BOOM/static/js/runtime-main.438d05c8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Phrasing-BOOM/precache-manifest.2299f8ed1bc236d7d0abe6370fd4cec4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Phrasing-BOOM/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
